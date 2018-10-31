9a1c5e432e124233bf9ec0d36f6111a95cc166ee
errorr404 <dkbishwash@gmail.com>
2018-10-31 22:25:25 +0530

    update todo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
+const _ = require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 
@@ -20,6 +21,7 @@ app.post('/todos',(req,res)=>{
   });
 });
 
+// get all todo
 app.get('/todos',(req,res)=>{
   Todo.find().then((todos)=>{
     res.send({todos});
@@ -28,6 +30,7 @@ app.get('/todos',(req,res)=>{
   });
 });
 
+// get individual todo
 app.get('/todos/:id',(req,res)=>{
   var id = req.params.id;
   if(!ObjectID.isValid(id)) {
@@ -43,6 +46,7 @@ app.get('/todos/:id',(req,res)=>{
   });
 });
 
+// delete todo
 app.delete('/todos/:id',(req,res)=>{
   var id = req.params.id;
   if(!ObjectID.isValid(id)) {
@@ -57,6 +61,33 @@ app.delete('/todos/:id',(req,res)=>{
     res.status(400).send();
   })
 });
+
+// update todo
+app.patch('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  var body = _.pick(req.body,['text','completed']);
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  if(_.isBoolean(body.completed)&& body.completed) {
+    body.completedAt = new Date().getTime();
+  }
+  else {
+    body.completed=false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id,{$set: body},{new: true}).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  })
+
+});
+
 app.listen(3000,()=>{
   console.log('Started on port 3000');
 });
